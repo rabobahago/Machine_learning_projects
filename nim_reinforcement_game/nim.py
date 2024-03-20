00f00587cc77805221e3ad8a90b7d1ef08293939
rabobahago <rabobahago@gmail.com>
2024-03-20 22:34:32 +0100

    nim reinforcement game available actions added

---
+++ nim_reinforcement_game/nim.py
@@ -0,0 +1,32 @@
+import math
+import random
+import time
+class Nim():
+    def __init__(self, initial=[1, 3, 5, 7]):
+        """
+        Initialize game board.
+        Each game board has
+            - `piles`: a list of how many elements remain in each pile
+            - `player`: 0 or 1 to indicate which player's turn
+            - `winner`: None, 0, or 1 to indicate who the winner is
+        """
+        self.piles = initial.copy()
+        self.player = 0
+        self.winner = None
+
+    @classmethod
+    def available_actions(cls, piles):
+        """
+        Nim.available_actions(piles) takes a `piles` list as input
+        and returns all of the available actions `(i, j)` in that state.
+
+        Action `(i, j)` represents the action of removing `j` items
+        from pile `i` (where piles are 0-indexed).
+        """
+        actions = set()
+
+        for i, pile in enumerate(piles):
+            for j in range(1, pile + 1):
+                actions.add((i, j))
+        return actions
+c = Nim()
