98d73a2f2cf24d8ef3c5582e7184bd3da4aba420
rabobahago <rabobahago@gmail.com>
2024-04-14 15:30:55 +0100

    forward pass function implemented

---
+++ build_neural_network/neural_network.py
@@ -0,0 +1,50 @@
+import numpy as np
+class NeuralNetwork:
+    """
+    A simple neural network with one hidden layer.
+
+    Parameters:
+    -----------
+    input_size: int
+        The number of input features
+    hidden_size: int
+        The number of neurons in the hidden layer
+    output_size: int
+        The number of neurons in the output layer
+    loss_func: str
+        The loss function to use. Options are 'mse' for mean squared error, 'log_loss' for logistic loss, and 'categorical_crossentropy' for categorical crossentropy.
+    """
+    def __init__(self, input_size, hidden_size, output_size, loss_func = 'mse'):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.loss_func = loss_func
+        # initialize weights and biases
+        self.weights1 = np.random.rand(self.input_size, self.hidden_size)
+        self.bias1 = np.ones((1, self.hidden_size))
+        self.weights2 = np.random.rand(self.hidden_size, self.output_size)
+        self.bias2 = np.ones((1, self.output_size))
+        # track the loss
+        self.train_loss = []
+        self.test_loss = []
+
+
+        def forward(self, X):
+            """
+            Perform forward propagation.
+
+            Parameters:
+            -----------
+            X: numpy array
+                The input data
+
+            Returns:
+            --------
+            numpy array
+                The predicted output
+            """
+            # Perform forward propagation
+            self.z1 = np.dot(X, self.weights1) + self.bias1
+            self.a1 = self.sigmoid(self.z1)
+            self.z2 = np.dot(self.z1, self.weights2)
+            self.a2 = self.sigmoid(self.z2)
