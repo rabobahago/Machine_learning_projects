b97d498866da5629a9f59375ad4ceaaf62c74b54
rabobahago <rabobahago@gmail.com>
2024-04-14 16:37:32 +0100

    neural network bug fixed

--- build_neural_network/neural_network.py
+++ build_neural_network/neural_network.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class NeuralNetwork:
     """
     A simple neural network with one hidden layer.
@@ -14,20 +15,21 @@ class NeuralNetwork:
     loss_func: str
         The loss function to use. Options are 'mse' for mean squared error, 'log_loss' for logistic loss, and 'categorical_crossentropy' for categorical crossentropy.
     """
-    def __init__(self, input_size, hidden_size, output_size, loss_func = 'mse'):
+    def __init__(self, input_size, hidden_size, output_size, loss_func='mse'):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.loss_func = loss_func
-        # initialize weights and biases
-        self.weights1 = np.random.rand(self.input_size, self.hidden_size)
-        self.bias1 = np.ones((1, self.hidden_size))
-        self.weights2 = np.random.rand(self.hidden_size, self.output_size)
-        self.bias2 = np.ones((1, self.output_size))
-        # track the loss
+
+        # Initialize weights and biases
+        self.weights1 = np.random.randn(self.input_size, self.hidden_size)
+        self.bias1 = np.zeros((1, self.hidden_size))
+        self.weights2 = np.random.randn(self.hidden_size, self.output_size)
+        self.bias2 = np.zeros((1, self.output_size))
+
+        # track loss
         self.train_loss = []
         self.test_loss = []
-
 
     def forward(self, X):
         """
@@ -46,13 +48,13 @@ class NeuralNetwork:
         # Perform forward propagation
         self.z1 = np.dot(X, self.weights1) + self.bias1
         self.a1 = self.sigmoid(self.z1)
-        self.z2 = np.dot(self.z1, self.weights2)
-        self.a2 = self.sigmoid(self.z2)
+        self.z2 = np.dot(self.a1, self.weights2) + self.bias2
         if self.loss_func == 'categorical_crossentropy':
             self.a2 = self.softmax(self.z2)
         else:
             self.a2 = self.sigmoid(self.z2)
         return self.a2
+
     def backward(self, X, y, learning_rate):
         """
         Perform backpropagation.
@@ -80,7 +82,9 @@ class NeuralNetwork:
             raise ValueError('Invalid loss function')
 
         self.dw2 = (1 / m) * np.dot(self.a1.T, self.dz2)
-        self.db2 = (1 / m) * np.sum(self.dz2, axis=0, keepdims=True)
+        # self.db2 = (1 / m) * np.sum(self.dz2, axis=0, keepdims=True)
+        self.db2 = (1 / m) * np.sum(self.dz2, axis=0).reshape(1, -1)
+
         self.dz1 = np.dot(self.dz2, self.weights2.T) * self.sigmoid_derivative(self.a1)
         self.dw1 = (1 / m) * np.dot(X.T, self.dz1)
         self.db1 = (1 / m) * np.sum(self.dz1, axis=0, keepdims=True)
@@ -122,6 +126,7 @@ class NeuralNetwork:
             The output of the derivative of the sigmoid function
         """
         return x * (1 - x)
+
     def softmax(self, x):
         """
         Softmax activation function.
