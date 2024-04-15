2a3e20eb2b31c845628bf4212969f00016fcbd58
rabobahago <rabobahago@gmail.com>
2024-04-15 11:47:37 +0100

    get_game, remember, train_long_memory, train_short_memory, get_action and train skeleton functions added

--- ai_snake_game/agent.py
+++ ai_snake_game/agent.py
@@ -6,3 +6,21 @@ from game import SnakeGameAI, Direction, Point
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
+LR = 0.001
+class Agent:
+    def __init__(self):
+        pass
+    def get_game(self,game):
+        pass
+    def remember(self, state, action, reward, next_state, done):
+        pass
+    def train_long_memory(self):
+        pass
+    def train_short_memory(self):
+        pass
+    def get_action(self, state):
+        pass
+def train():
+    pass
+if __name__ == '__main__':
+    train()
