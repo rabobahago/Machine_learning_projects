dfc5f442ddff9d9bffed1771d538e6019e120d51
rabobahago <rabobahago@gmail.com>
2024-04-15 14:45:22 +0100

    ai_snake_game completed

--- ai_snake_game/agent.py
+++ ai_snake_game/agent.py
@@ -1,20 +1,26 @@
-import numpy as np
-import random
 import torch
+import random
+import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+
 class Agent:
+
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0
-        self.gamma = 0
-        self.memory = deque(maxlen=MAX_MEMORY)
-        self.model = None
-        self.trainer = None
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+
     def get_state(self, game):
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
@@ -60,8 +66,10 @@ class Agent:
             ]
 
         return np.array(state, dtype=int)
+
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+
     def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
             mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
@@ -70,8 +78,12 @@ class Agent:
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
+
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
+
     def get_action(self, state):
         # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
@@ -86,6 +98,8 @@ class Agent:
             final_move[move] = 1
 
         return final_move
+
+
 def train():
     plot_scores = []
     plot_mean_scores = []
@@ -94,24 +108,40 @@ def train():
     agent = Agent()
     game = SnakeGameAI()
     while True:
+        # get old state
         state_old = agent.get_state(game)
+
+        # get move
         final_move = agent.get_action(state_old)
+
+        # perform move and get new state
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
+
+        # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
         agent.remember(state_old, final_move, reward, state_new, done)
+
         if done:
+            # train long memory, plot result
             game.reset()
             agent.n_games += 1
             agent.train_long_memory()
+
             if score > record:
                 record = score
-            print('Game: ', agent.n_games, 'Score: ', score, 'Record: ', record)
+                agent.model.save()
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
             plot_scores.append(score)
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
+
 
 if __name__ == '__main__':
     train()
